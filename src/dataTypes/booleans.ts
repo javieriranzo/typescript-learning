71d64b7ac83f6179f629230c347558d4b6d46095
Javier Iranzo <jiranzo@iti.es>
2025-09-24 13:56:32 +0200

    Delete files without operations. Create files for testing data types with operations

---
+++ src/dataTypes/booleans.ts
@@ -0,0 +1,7 @@
+let knowJavascript: boolean = true; 
+
+if (knowJavascript) {
+    console.log("You can continue watching this course")
+} else {
+    console.log("You must study Javascript first")
+}
